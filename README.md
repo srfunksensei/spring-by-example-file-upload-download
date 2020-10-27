4274fb61bcd92e7ac1189a74175bacf70e2103dc
srfunksensei <sr.funk.sensei@gmail.com>
2020-10-27 10:32:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring by Example (File Upload/Download)
+
+Server application that can receive HTTP multi-part file uploads with additional model.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ src/main/java/com/mb/springfileupload/SpringFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.mb.springfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileUploadApplication.class, args);
+	}
+
+}
